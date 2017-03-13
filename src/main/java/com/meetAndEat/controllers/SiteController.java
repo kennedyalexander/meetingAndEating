5f5068282bcd697307895a637d26381bde7fd256
Alexander <alexanderkennedy@hotmail.co.uk>
2017-03-13 18:16:35 +0100

    Able to add events. now need to be able to get them.

--- src/main/java/com/meetAndEat/controllers/SiteController.java
+++ src/main/java/com/meetAndEat/controllers/SiteController.java
@@ -2,6 +2,7 @@ package com.meetAndEat.controllers;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -9,12 +10,15 @@ import java.util.UUID;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.meetAndEat.dao.UserDao;
 import com.meetAndEat.dao.UserDaoImpl;
 import com.meetAndEat.dtos.EventDto;
@@ -107,28 +112,38 @@ public class SiteController {
 	
 
 	
-	@PostMapping("/createEvent")
+	@PostMapping("/createEvent" )
 	@ResponseStatus(value = HttpStatus.OK)
-	public void createEvent( Principal principal, @ModelAttribute EventDto eventDto) {
+	public void createEvent( Principal principal, @ModelAttribute EventDto eventDto ) {
 		Event event = eventDtoToEvent(eventDto, principal.getName(), EventSatus.PENDING);
 		eventCreationServiceImpl.createEvent(event);
 		System.out.println("Created");
 	}
 	
-	@GetMapping("/getEventsForUser")
+	@GetMapping("/getEventsPage")
 	@ResponseStatus(value = HttpStatus.OK)
-	public ModelAndView getEventsForUser(Principal principal) {
-	List<Event> events = eventCreationServiceImpl.getUsersEvents(principal.getName());
+	public ModelAndView getEventPage(Principal principal) {
 		ModelAndView model = new ModelAndView();
-		model.setViewName("userProfilePage");
-		model.addObject("username", events.get(0).getHost());
-		model.addObject("firstName", events.get(0).getEventDate());
+		model.setViewName("userEvents");
+		model.addObject("username", principal.getName());		
 		return model;
+	}
+	
+	@PostMapping("/getEventsForUser")
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<Event> getEventsForUser(Principal principal) {
+	List<Event> events = eventCreationServiceImpl.getUsersEvents(principal.getName());
+		return events;
 	}
 	
 	public Event eventDtoToEvent(EventDto eventDto, String creator, EventSatus eventStatus){
 		return new Event(UUID.randomUUID().toString(), eventDto.getEventName(),eventDto.eventDate, eventDto.minimumGuests, eventDto.maximumGuests, eventDto.location, creator, eventDto.food
 				, eventStatus);
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true, 10));
 	}
 	
 
